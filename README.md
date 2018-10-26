ea8df688abbd873dfe19da4fdd0b1e67e2761e72
Farrah Yee <classprojectjn@gmail.com>
2018-10-26 01:30:34 +0000

    Final

--- README.md
+++ README.md
@@ -33,26 +33,26 @@ In this unit, coders will create a Rock, Paper, Scissors game. The Rock, Paper, 
 - [x] In your click handler, store the input value in the userChoice variable	
 - [x] In your click handler, display userChoice to the screen
 #### Wrap
-- [ ] Push your changes!
+- [x] Push your changes!
 
 ## Day 3
 
 ## JavaScript
-- [ ] Write a conditional statement which, given the number range of randomNumber, assigns ‘rock’, ‘paper’ or ‘scissors’ to the computerChoice variable 
-- [ ] Display the computerChoice to the screen
-- [ ] Write a conditional statement that compares the userChoice to the computerChoice to determine the winner of the game
-- [ ] Display the winner to the screen in the result div
-- [ ] Clear the input value once a result is displayed
+- [x] Write a conditional statement which, given the number range of randomNumber, assigns ‘rock’, ‘paper’ or ‘scissors’ to the computerChoice variable 
+- [x] Display the computerChoice to the screen
+- [x] Write a conditional statement that compares the userChoice to the computerChoice to determine the winner of the game
+- [x] Display the winner to the screen in the result div
+- [x] Clear the input value once a result is displayed
 
 #### Wrap
-- [ ] Push your changes!
+- [x] Push your changes!
 
 ## Projects Extensions:
 - [x] Style the page to fit your personality
-- [ ] Validates input so that it will return “Not valid input” if the user types in a wrong choice. 
-- [ ] Accepts any form of a word regardless of capitalization (i.e. “Rock” “rock” roCk”)
-- [ ] Keeps track of total wins and losses, until the page refreshes. 
-- [ ] Create a game with more variety in throwing options. Example: [Rock-Paper-Scissors-Lizard-Spock](http://en.wikipedia.org/wiki/Rock-paper-scissors-lizard-Spock)
+- [x] Validates input so that it will return “Not valid input” if the user types in a wrong choice. 
+- [x] Accepts any form of a word regardless of capitalization (i.e. “Rock” “rock” roCk”)
+- [x] Keeps track of total wins and losses, until the page refreshes. 
+- [x] Create a game with more variety in throwing options. Example: [Rock-Paper-Scissors-Lizard-Spock](http://en.wikipedia.org/wiki/Rock-paper-scissors-lizard-Spock)
 
 ## References/Tools
 * [Advanced Reference Table]()
--- js/script.js
+++ js/script.js
@@ -5,11 +5,18 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var randomNumber = 0;
+var userScore = 0;
+var computerScore = 0;
+
 
 
 // DOCUMENT READY FUNCTION BELOW
 $("button").click(function(){
-    var userChoice = $("input").val();
+    var userChoice = $("input").val().toLowerCase();
     $("#userChoice").text(userChoice);
     var randomNumber = Math.random();
     
@@ -21,12 +28,47 @@ $("button").click(function(){
     computerChoice = "scissors";
 }
     $("#computerChoice").text(computerChoice);
+    
+    if (computerChoice === "rock" && userChoice === "scissors") {
+        var winner = "Computer wins!";
+        computerScore = computerScore + 1;
+        $("#result").text(winner);
+    } else if (computerChoice === "paper" && userChoice === "scissors") {
+        var winner = "User wins!";
+        userScore = userScore + 1;
+        $("#result").text(winner);
+    } else if (computerChoice === "paper" && userChoice === "rock") {
+        var winner = "Computer wins!";
+        computerScore = computerScore + 1;
+        $("#result").text(winner);
+    } else if (computerChoice === "scissors" && userChoice === "rock") {
+        var winner = "User wins!";
+        userScore = userScore + 1;
+        $("#result").text(winner);
+    } else if (computerChoice === "scissors" && userChoice === "paper") {
+        var winner = "Computer wins!";
+        computerScore = computerScore + 1;
+        $("#result").text(winner);
+    } else if (computerChoice === "rock" && userChoice === "paper") {
+        var winner = "User wins!";
+        userScore = userScore + 1;
+        $("#result").text(winner);
+    } else if(computerChoice === userChoice) {
+        var winner = "Tie!";
+        $("#result").text(winner);
+    } else {
+        var winner = "Not valid";
+        $("#result").text(winner);
+    }
+    
+    $("#userScore").text("Score: " + userScore);
+    $("#computerScore").text("Score: " + computerScore);
+    
+    if (userScore >= 10) {
+        alert("The user is the overall winner!");
+    } else if (computerScore >= 10) {
+        alert("The computer is the overall winner!");
+    }
 });
 
-var userChoice = "";
-var computerChoice = "";
-var winner = "";
-var randomNumber = 0;
-var userScore = 0;
-var computerScore = 0;
 
